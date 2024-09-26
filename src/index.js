428cb10115ffe1b9e314f1b160c349c352c665ac
Madeline Conway <conway.maddy@yahoo.com>
2024-09-26 14:25:52 -0400

    m

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import{createBrowserRouter, RouterProvider} from 'react-router-dom';
+import routes from './routes';
+
+
+const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render( <RouterProvider router={router}/>)
+
+  (<React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {ErrorPage} from 'react'
+
+const routes = [
+{
+    element: 
+    path:
+}
+{}
+]
+
+
+export default routes
